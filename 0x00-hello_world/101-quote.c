0cfdf6ae34c0a19267b8c8c7ee8ccd52534244ad
nlahkimdev <nlahkimdev@gmail.com>
2022-12-08 06:10:01 -0800

    fix 6:print a text in c without using printf or puts

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,8 +9,9 @@
 int main(void)
 {
 size_t i = 0;
-char str[59] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+char str[59] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 for (i = 0 ; i < strlen(str); ++i)
 	putchar(str[i]);
+putchar('\n');
 return (1);
 }
