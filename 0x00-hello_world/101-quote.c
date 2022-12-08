8294b3a5a66b22b6794edea9e91999c2a45707f8
nlahkimdev <nlahkimdev@gmail.com>
2022-12-08 05:04:36 -0800

    #include <string.h>

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i, n = 0;
+char str[59] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+for (i = 0; i < strlen(str); ++i)
+	putchar(str[i]);
+return (1);
+}
