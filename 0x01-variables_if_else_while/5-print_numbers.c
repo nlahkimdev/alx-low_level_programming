755d8979e5f620a43fbaca7e51abe72c63dfb30e
nlahkimdev <nlahkimdev@gmail.com>
2022-12-10 07:11:43 -0800

    prints numbers from 0 to 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n = 0;
+
+	for (n = 0; n <= 10; n++)
+	{
+		printf("%i\n", n);
+	}
+	return (0);
+}
