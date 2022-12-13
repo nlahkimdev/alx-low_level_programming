9d13e6c28632421fc2821c90ec13891de855fc44
nlahkimdev <nlahkimdev@gmail.com>
2022-12-13 02:16:48 -0800

    fix 3:a scipt that prints _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,9 +7,9 @@
  */
 int main(void)
 {
-	int myString[] = {95, 112,117, 116, 99, 104, 97, 114};
+	int myString[] = {95, 112, 117, 116, 99, 104, 97, 114};
 	int i;
-	
+
 	for (i = 0; i < 8; i++)
 	{
 		putchar(myString[i]);
