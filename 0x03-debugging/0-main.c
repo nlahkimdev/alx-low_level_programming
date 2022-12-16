b3074fb4e236194b0f37ae5f0003dd6f36f63dae
nlahkimdev <nlahkimdev@gmail.com>
2022-12-15 18:53:20 -0800

    fix 2:debug the positive and negative c code

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -3,15 +3,13 @@
 /**
  * main - Entry point
  *
- *@i: a given number
- *
  * Return: Always 0 (Success)
  */
- int main(void i)
+ int main(void)
 {
-	/* int i; */
+	int i;
 
-        /* i = 0; */
+        i = 0;
         positive_or_negative(i);
         return (0);
 }
