0baf7192b03ca2089316d7b522c48616648eae51
nlahkimdev <nlahkimdev@gmail.com>
2022-12-16 19:41:02 -0800

    fix 1:a function that prints 10 times the numbers from 0 to 1

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,16 +9,16 @@ void more_numbers(void)
 {
 	int i;
 	int j;
+	int num[] = {48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 49, 50, 51, 52};
 
 	j = 0;
 	while (j < 10)
 	{
-		for (i = 48; i < 58; i++)
-			_putchar(i);
-		for (i = 48; i < 53; i++)
+		for (i = 0; i < 15 ; i++)
 		{
-			_putchar(49);
-			_putchar(i);
+			if (i > 9)
+				_putchar(49);
+		_putchar(num[i]);
 		}
 		_putchar('\n');
 	j++;
