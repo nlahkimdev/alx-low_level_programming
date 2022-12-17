4043f70ea98dcbb61a6a77b6631c36e9000ec8c3
nlahkimdev <nlahkimdev@gmail.com>
2022-12-16 17:40:39 -0800

    The Fizz-Buzz test

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - the fizz buzz program
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int a;
+	char fizz[] = "Fizz";
+	char buzz[] = "Buzz";
+	char fizzBuzz[] = "FizzBuzz";
+
+	for (a = 1; a < 101; a++)
+	{
+		if (a % 3 == 0)
+			printf("%s", fizz);
+		else if (a % 5 == 0)
+			printf("%s", buzz);
+		else if (a % 3 == 0 && a % 5 == 0)
+			printf("%s", fizzBuzz);
+		else
+			printf("%i", a);
+	}
+	printf("%c", '\n');
+	return (0);
+}
