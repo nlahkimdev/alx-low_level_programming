258020b5faf95dc8f05a731feef723fcee0e04a2
nlahkimdev <nlahkimdev@gmail.com>
2022-12-16 17:05:28 -0800

    fix 1:a function that prints a square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -12,13 +12,18 @@ void print_square(int size)
 	int i;
 	int j;
 
-	for (i = 0; i < size; i++)
+	if (size > 0)
 	{
-		_putchar(35);
-		for (j = 1; j < size; j++)
+		for (i = 0; i < size; i++)
 		{
 			_putchar(35);
+			for (j = 1; j < size; j++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
+	else
+		_putchar('\n');
 }
