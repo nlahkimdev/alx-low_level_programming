722be66a6a35f440e9e94bf72643342d84449d96
nlahkimdev <nlahkimdev@gmail.com>
2022-12-22 17:34:46 -0800

    fix 1:a function that capitalizes all words of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,32 +1,34 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * cap_string - capitalize all words
+ *
+ * @str: a string
  *
  * Return: Always 0.
  */
 char *cap_string(char *str)
 {
 	int i = 0;
+	int j;
+	char sep[] = " \t\n,;.!?\"(){}";
 
-	while (str[++i])
+	while (*(str + i))
 	{
-		while (!(str[i] >= 'a' && str[i] <= 'z'))
-			i++;
-		if (str[i - 1] == ' ' ||
-				str[i - 1] == '\t' ||
-				str[i - 1] == '\n' ||
-				str[i - 1] == ',' ||
-				str[i - 1] == ';' ||
-				str[i - 1] == '.' ||
-				str[i - 1] == '!' ||
-				str[i - 1] == '?' ||
-				str[i - 1] == '"' ||
-				str[i - 1] == '(' ||
-				str[i - 1] == ')' ||
-				str[i - 1] == '{' ||
-				str[i - 1] == '}')
-			str[i] -= 32;
+		if (*(str + i) >= 'a' && *(str + i) <= 'z')
+		{
+			if (i == 0)
+				*(str + i) -= 'a' - 'A';
+			else
+			{
+				for (j = 0; j <= 12; j++)
+				{
+					if (sep[j] == *(str + i - 1))
+						*(str + i) -= 'a' - 'A';
+				}
+			}
+		}
+		i++;
 	}
 	return (str);
 }
