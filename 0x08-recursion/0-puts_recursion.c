8081f287db98a8b0b0ab4418a9761da418107a20
Replit user <>
2023-11-12 14:53:36 +0000

    fix 4: 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,7 +3,9 @@
 /**
  * _puts_recursion - a function that prints a string, followed by a new line
  *
- * Return: return nothing.
+ * @s: string to print.
+ *
+ * Return: void.
  */
 void _puts_recursion(char *s)
 {
