df67a2645034d02effb2724e3589b2b23f7b736c
Replit user <>
2023-11-12 14:49:07 +0000

    fix: 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,10 +7,15 @@
  */
 void _puts_recursion(char *s)
 {
-	while(*s != '\0')
+	if(*s != '\0')
 	{
 		_putchar(*s);
 		s++;
 	}
-	_putchar('\n');
+	else
+	{
+		_putchar('\n');
+		return;
+	}
+	_puts_recursion(s);
 }
