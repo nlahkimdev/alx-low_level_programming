71139dc0f061d1a40df4a0cf3d55e864e9169f00
Replit user <>
2023-11-14 07:55:03 +0000

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /*
- * main - entry point
+ * main : entry point
  *
  * @argc - arguments count
  *
@@ -12,7 +12,6 @@
 int main(int argc, char *argv[])
 {
 	(void) argc;
-	
 	printf("%s\n", argv[0]);
 	return (0);
 }
