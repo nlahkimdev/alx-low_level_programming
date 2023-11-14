811401198286c902c87f4c048c9ae89588ff38cf
Replit user <>
2023-11-14 13:15:46 +0000

    fix:0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,6 +17,9 @@ char *create_array(unsigned int size, char c)
 	
 	if (arr == NULL)
 		return NULL;
+	// if size 0 return NULL
+	if (size == 0)
+		return (NULL);
 	
 	for (i = 0; i < size; i++)
 		arr[i] = c;
