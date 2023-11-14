344ee60955de73f122ec1fe0a8aa11f70f703f07
Replit user <>
2023-11-14 08:13:50 +0000

    2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - prints all argumetns, one per line
+ *
+ * @argc : arguments count
+ *
+ * @argv : arguments vector
+ *
+ * Return: 0 if success, anything else if error
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
