02290960c0bdb8e6843556695a9b17f959a07380
Replit user <>
2023-11-14 16:02:53 +0000

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -9,7 +9,7 @@
  */
 char *_strdup(char *str)
 {
-	/* get sizeo of str*/
+	/* get sizeo of str */
 	int size = strlen(str);
 	int i;
 	char *new_str;
@@ -17,14 +17,18 @@ char *_strdup(char *str)
 	if (str == NULL)
 		return (NULL);
 	/* allocate memory for str */
-	new_str = malloc(size * sizeof(char));
-
+	new_str = malloc((size + 1) * sizeof(char));
+	/* handle no enough memory available */
 	if (new_str == NULL)
 		return (NULL);
 	/* if size 0 return NULL */
 	if (new_str == 0)
 		return (NULL);
+	/* copy the string into the dynamically allicated variable */
 	for (i = 0; i < size; i++)
 		new_str[i] = str[i];
+	/* add the null character at the end of the copied string */
+	new_str[size] = '\0';
+	
 	return (new_str);
 }
