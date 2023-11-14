3a0c74f22af9a17d6bda04b49cc72a18411d8a40
Replit user <>
2023-11-14 15:01:44 +0000

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,11 +16,11 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	if (arr == NULL)
-		return NULL;
+		return (NULL);
 	/* if size 0 return NULL */
 	if (size == 0)
 		return (NULL);
 	for (i = 0; i < size; i++)
 		arr[i] = c;
-	return arr;
+	return (arr);
 }
